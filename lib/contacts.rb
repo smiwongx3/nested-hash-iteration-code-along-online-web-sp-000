ffd0e26542f8836c45b5a68f8f4bf25fa0031d2f
Samantha Wong <smiwong920@gmail.com>
2019-02-24 15:35:35 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,7 +15,6 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     contact_details_hash.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
